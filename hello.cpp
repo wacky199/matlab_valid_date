54b3ccfe823c08bc0648e2056585db54314b0304
wacky199 <shivamjio01111999@gmail.com>
2020-06-26 16:15:06 +0530

    made a new branch code2

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,7 @@ int main()
     cout<<"Hello_World!!!";
     cout<<"I'm just making a change here.";
     cout<<"I'll try pull now.";
+    cout<<"This is going to be add in code2 branch";
+    cout<<"Lets check!!";
     return 0;
 }
